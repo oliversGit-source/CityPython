9caf88101b78bdbef5c1955fc77620c32ec9e890
oliversGit-source <obutterworthbakhshi@icloud.com>
2026-01-25 15:29:35 +0000

    Greeter function added.

---
+++ week 1/greeter.py
@@ -0,0 +1,18 @@
+def greet_user(name):
+    '''Display a simple greeting.'''
+    print(f'Greetings, {name}')
+
+
+def formatted_name(firstName, lastName):
+    '''Greet user by full name, neatly formatted'''
+    fullName = f'{firstName} {lastName}!'
+    return fullName
+
+
+def main():
+    name = input('What is your name?').split()
+    greet_user(' '.join(name))
+    name2 = formatted_name(name[0], name[1])
+    print(f'Hello {name2}!')
+
+main()
