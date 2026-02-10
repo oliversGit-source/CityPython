8efc74093a6acf444e75ac01add43674a9282ba8
oliversGit-source <obutterworthbakhshi@icloud.com>
2026-02-10 19:26:41 +0000

    Fixed NLP file.

--- week 3/NLP.py
+++ week 3/NLP.py
@@ -20,7 +20,7 @@ def wordcount(word):
         '\n'.join(fullText)
     for x in fullText:
         foundlist.append(x.find(word))
-        count += 2
+        count += 1
     print(f'The number of times {word} appeared in the search was {count}')
 
 wordcount('heart')
